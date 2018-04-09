4b8f502d2d8f83ac423549e18b3ed4518ead8aee
David Librera <davidlibrera@gmail.com>
2018-04-09 10:32:07 +0200

    Not run rubocop on schema dumping

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -10,54 +10,32 @@ RSpec.describe 'Fix DB Schema Conflicts' do
 
     generated_lines = File.readlines('spec/test-app/db/schema.rb')
 
-    generated_lines.zip(expected_lines).each do |generated, expected|
-      next if expected.nil?
-      next if expected.start_with?('ActiveRecord::Schema.define')
-      expect(generated).to eq(expected)
+    column_list = generated_lines.select do |line|
+      line.strip.start_with?("t.")
     end
+
+    column_names = column_list.map do |line|
+      line.split(",")[0].strip.split[-1].gsub(/"/, "")
+    end
+
+    expected_names = [
+      "addr1",
+      "addr2",
+      "city",
+      "created_at",
+      "name",
+      "phone",
+      "state",
+      "updated_at",
+      "zip",
+      "age",
+      "birthdate",
+      "created_at",
+      "first_name",
+      "last_name",
+      "updated_at"
+    ]
+
+    expect(column_names).to match expected_names
   end
-end
-
-def reference_db_schema
-  <<-RUBY
-# encoding: UTF-8
-
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20160322223258) do
-  create_table "companies", force: :cascade do |t|
-    t.string "addr1"
-    t.string "addr2"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.string "name"
-    t.string "phone"
-    t.string "state"
-    t.datetime "updated_at", null: false
-    t.string "zip"
-  end
-
-  add_index "companies", ["city"], name: "index_companies_on_city"
-  add_index "companies", ["name"], name: "index_companies_on_name"
-  add_index "companies", ["state"], name: "index_companies_on_state"
-
-  create_table "people", force: :cascade do |t|
-    t.integer "age"
-    t.date "birthdate"
-    t.datetime "created_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "updated_at", null: false
-  end
-  RUBY
 end
--- spec/test-app/db/schema.rb
+++ spec/test-app/db/schema.rb
@@ -1,5 +1,4 @@
 # encoding: UTF-8
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,16 +12,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160322223258) do
+
   create_table "companies", force: :cascade do |t|
-    t.string "addr1"
-    t.string "addr2"
-    t.string "city"
+    t.string   "addr1"
+    t.string   "addr2"
+    t.string   "city"
     t.datetime "created_at", null: false
-    t.string "name"
-    t.string "phone"
-    t.string "state"
+    t.string   "name"
+    t.string   "phone"
+    t.string   "state"
     t.datetime "updated_at", null: false
-    t.string "zip"
+    t.string   "zip"
   end
 
   add_index "companies", ["city"], name: "index_companies_on_city"
@@ -30,11 +30,12 @@ ActiveRecord::Schema.define(version: 20160322223258) do
   add_index "companies", ["state"], name: "index_companies_on_state"
 
   create_table "people", force: :cascade do |t|
-    t.integer "age"
-    t.date "birthdate"
+    t.integer  "age"
+    t.date     "birthdate"
     t.datetime "created_at", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "updated_at", null: false
   end
+
 end
