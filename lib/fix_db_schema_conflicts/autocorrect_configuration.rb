61b941109bc5aaf5ca8f19e12ff9ab19d1d7250e
Alistair McKinnell <alistairm@nulogy.com>
2018-05-15 01:57:54 -0400

    Add option to choose configuration for Rubocop 0.53.0

--- lib/fix_db_schema_conflicts/autocorrect_configuration.rb
+++ lib/fix_db_schema_conflicts/autocorrect_configuration.rb
@@ -5,13 +5,23 @@ module FixDBSchemaConflicts
     end
 
     def load
-      at_least_rubocop_49? ? '.rubocop_schema.49.yml' : '.rubocop_schema.yml'
+      if at_least_rubocop_53?
+        '.rubocop_schema.53.yml'
+      elsif at_least_rubocop_49?
+        '.rubocop_schema.49.yml'
+      else
+        '.rubocop_schema.yml'
+      end
     end
 
     private
 
     def at_least_rubocop_49?
       Gem::Version.new('0.49.0') <= Gem.loaded_specs['rubocop'].version
+    end
+
+    def at_least_rubocop_53?
+      Gem::Version.new('0.53.0') <= Gem.loaded_specs['rubocop'].version
     end
   end
 end
