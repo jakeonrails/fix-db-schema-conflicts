59bb603909cb66b4bf13935033d5848389935583
Ross Cooperman <cooperman@gmail.com>
2018-08-17 10:02:13 -0400

    As of 0.50.0 rubocop also no longer wants encoding headers

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -20,8 +20,6 @@ end
 
 def reference_db_schema
   <<-RUBY
-# encoding: UTF-8
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
